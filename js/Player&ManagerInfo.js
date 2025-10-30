55cbfddf977e83ebb7680fa829aa3b91c0e827ea
L AS <as125230as@gmail.com>
2025-10-31 00:13:53 +0800

    把所有頁面的Footer內所 有連結都補好修正好;製作好Footer內連結YT 的頁面(球隊邀請);製作好球經資訊頁面;UI修整(未完成)，UI修正好就準備加入動畫了。

--- js/Player&ManagerInfo.js
+++ js/Player&ManagerInfo.js
@@ -2,7 +2,7 @@ window.addEventListener("scroll", function () {
     const header = document.getElementById("SecHeader");
     if (window.scrollY > 50) {
         header.classList.add("shrink");
-    } 
+    }
     else {
         header.classList.remove("shrink");
     }
