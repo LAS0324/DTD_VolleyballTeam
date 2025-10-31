d492aac4d341acdecda55da934cc413153d5542c
L AS <as125230as@gmail.com>
2025-11-01 00:13:21 +0800

    修正排版與顏色搭配;選手頁面動畫卡關，不知道怎麼才能hover時有圖片放大效果，同時hover寫的效果不會被動畫卡掉。目前是讓圖片在div外框內放大(上次忘記寫已經有給圖片加hover時選手框放大的效果)

--- js/Players.js
+++ js/Players.js
@@ -2,9 +2,72 @@ window.addEventListener("scroll", function () {
     const header = document.getElementById("SecHeader");
     if (window.scrollY > 50) {
         header.classList.add("shrink");
-    } 
+    }
     else {
         header.classList.remove("shrink");
     }
 });
+
+// const AnimeRemove1 = document.querySelector(".main-photo-animation1");
+// const AnimeRemove2 = document.querySelector(".main-photo-animation2");
+// const AnimeRemove3 = document.querySelector(".main-photo-animation3");
+// const AnimeRemove4 = document.querySelector(".main-photo-animation4");
+// const AnimeRemove5 = document.querySelector(".main-photo-animation5");
+// const AnimeRemove6 = document.querySelector(".main-photo-animation6");
+// const AnimeRemove7 = document.querySelector(".main-photo-animation7");
+// const AnimeRemove8 = document.querySelector(".main-photo-animation8");
+// const AnimeRemove9 = document.querySelector(".main-photo-animation9");
+// const AnimeRemove10 = document.querySelector(".main-photo-animation10");
+// const AnimeRemove11 = document.querySelector(".main-photo-animation11");
+// const AnimeRemove12 = document.querySelector(".main-photo-animation12");
+// const AnimeRemove13 = document.querySelector(".main-photo-animation13");
+// const AnimeRemove14 = document.querySelector(".main-photo-animation14");
+// const OpacityBack = document.querySelector(".opacity-back");
+
+
+// AnimeRemove1.addEventListener("animationend", function () {
+//     AnimeRemove1.classList.remove("main-photo-animation1");
+//     OpacityBack.classList.add("main-photo.new");
+
+// });
+// AnimeRemove2.addEventListener("animationend", function () {
+//     AnimeRemove2.classList.remove("main-photo-animation2");
+// });
+// AnimeRemove3.addEventListener("animationend", function () {
+//     AnimeRemove3.classList.remove("main-photo-animation3");
+// });
+// AnimeRemove4.addEventListener("animationend", function () {
+//     AnimeRemove4.classList.remove("main-photo-animation4");
+//     OpacityBack.classList.add("main-photo.new");
+// });
+// AnimeRemove5.addEventListener("animationend", function () {
+//     AnimeRemove5.classList.remove("main-photo-animation5");
+// });
+// AnimeRemove6.addEventListener("animationend", function () {
+//     AnimeRemove6.classList.remove("main-photo-animation6");
+// });
+// AnimeRemove7.addEventListener("animationend", function () {
+//     AnimeRemove7.classList.remove("main-photo-animation7");
+// });
+// AnimeRemove8.addEventListener("animationend", function () {
+//     AnimeRemove8.classList.remove("main-photo-animation8");
+// });
+// AnimeRemove9.addEventListener("animationend", function () {
+//     AnimeRemove9.classList.remove("main-photo-animation9");
+// });
+// AnimeRemove10.addEventListener("animationend", function () {
+//     AnimeRemove10.classList.remove("main-photo-animation10");
+// });
+// AnimeRemove11.addEventListener("animationend", function () {
+//     AnimeRemove11.classList.remove("main-photo-animation11");
+// });
+// AnimeRemove12.addEventListener("animationend", function () {
+//     AnimeRemove12.classList.remove("main-photo-animation12");
+// });
+// AnimeRemove13.addEventListener("animationend", function () {
+//     AnimeRemove13.classList.remove("main-photo-animation13");
+// });
+// AnimeRemove14.addEventListener("animationend", function () {
+//     AnimeRemove14.classList.remove("main-photo-animation14");
+// });
 
